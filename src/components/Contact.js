c0e2df6ac18d47b845c1f171899a029be4a272e2
dvnsith <devinsith92@gmail.com>
2023-02-06 11:44:40 -0800

    fixed contact message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -73,7 +73,7 @@ export default function Contact() {
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Let's connect!
+            Let's connect
           </h2>
           <p className="leading-relaxed mb-5">
           Reach out to learn more about how my skillset can benefit your business. 
