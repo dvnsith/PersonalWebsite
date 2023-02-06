e054dea4ee904a49b1edc68c99b072efde4ce7f3
dvnsith <devinsith92@gmail.com>
2023-02-06 14:30:44 -0800

    added experience to skills section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,13 @@ import { projects } from "../data";
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container px-5 py-10 mx-auto lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white text-center">
             Apps
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-center">
             I'm learning to build more apps with Java and Spring Boot at the moment. Updates coming soon!
           </p>
         </div>
@@ -23,18 +23,20 @@ export default function Projects() {
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
                 <img
-                  alt="More apps to come!"
+                  alt="apps"
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1 text-center">
                     {project.subtitle}
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="title-font text-lg font-medium text-white mb-3 text-center">
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p>
+                  <br/>
+                  <p>{project.linkMessage}: {project.link}</p>
                 </div>
               </div>
             </a>
